2ea7fe34ac2bbf6a8210fee171d32625fd05d50e
sureshHARDIYA <itsmeskm99@gmail.com>
2018-10-12 13:53:52 +0200

    Push something in chapter 3

---
+++ CH03/jsonld.js
@@ -0,0 +1,13 @@
+<script type="application/ld+json">
+{
+  "@context": "http://schema.org",
+  "@type": "Organization",
+  "url": "http://www.example.com",
+  "name": "Unlimited Ball Bearings Corp.",
+  "contactPoint": {
+    "@type": "ContactPoint",
+    "telephone": "+1-401-555-1212",
+    "contactType": "Customer service"
+  }
+}
+</script>
